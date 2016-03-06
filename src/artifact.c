00d8661d898734e44905408ca02f224ad8e2acbe
tanguband <tanguband@gmail.com>
2016-03-07 00:12:01 +0900

    ランダムアーティファクト出現率　調整

--- src/artifact.c
+++ src/artifact.c
@@ -14,12 +14,12 @@
 
 
 /* Chance of using syllables to form the name instead of the "template" files */
-#define SINDARIN_NAME   10 /*!< ランダムアーティファクトにシンダリン銘をつける確率 */
-#define TABLE_NAME      20 /*!< ランダムアーティファクトに漢字銘をつける確率(正確には TABLE_NAME - SINDARIN_NAME %)となる */
-#define A_CURSED        13 /*!< 1/nの確率で生成の巻物以外のランダムアーティファクトが呪いつきになる。 */
-#define WEIRD_LUCK      12 /*!< 1/nの確率でrandom_resistance()の処理中バイアス外の耐性がつき、create_artifactで4を超えるpvalが許可される。*/
-#define BIAS_LUCK       20 /*!< 1/nの確率でrandom_resistance()で付加する元素耐性が免疫になる */
-#define IM_LUCK         7 /*!< 1/nの確率でrandom_resistance()で複数免疫の除去処理が免除される */
+#define SINDARIN_NAME   90 /*!< ランダムアーティファクトにシンダリン銘をつける確率 */ /* #tang 10 -> 90*/
+#define TABLE_NAME      95 /*!< ランダムアーティファクトに漢字銘をつける確率(正確には TABLE_NAME - SINDARIN_NAME %)となる */ /* #tang 20 -> 95*/
+#define A_CURSED        52 /*!< 1/nの確率で生成の巻物以外のランダムアーティファクトが呪いつきになる。 */ /* #tang 13 -> 52*/
+#define WEIRD_LUCK      6 /*!< 1/nの確率でrandom_resistance()の処理中バイアス外の耐性がつき、create_artifactで4を超えるpvalが許可される。*/ /* #tang 12 -> 6*/
+#define BIAS_LUCK       10 /*!< 1/nの確率でrandom_resistance()で付加する元素耐性が免疫になる */ /* #tang 20 -> 10*/
+#define IM_LUCK         4 /*!< 1/nの確率でrandom_resistance()で複数免疫の除去処理が免除される */ /* #tang 7 -> 4*/
 
 /*! @note
  * Bias luck needs to be higher than weird luck,
@@ -1835,7 +1835,7 @@ bool create_artifact(object_type *o_ptr, bool a_scroll)
 
 	if (a_cursed) powers /= 2;
 
-	max_powers = powers;
+	max_powers = powers * 5; /* #tang powers -> powers * 5 */
 	/* Main loop */
 	while (powers--)
 	{
